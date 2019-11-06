f051318c052c5d8a9481aed8f03cee93cb103db4
saki_bloom <toki.ryuka.senriyama@gmail.com>
2019-11-06 12:26:35 +0900

    20191106

---
+++ VR D/Assets/Scripts/GameObjects/Bowl.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bowl : MonoBehaviour
+{
+    /// <summary>
+    /// 投入物のIDを格納
+    /// 例)砂糖、黒糖.etc)
+    /// </summary>
+    Queue<uint> contentsID;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void Charge()
+    {
+
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        Powder powder;
+        if (other.TryGetComponent(out powder))
+        {
+            ThrowIn(powder);
+        }
+    }
+
+    /// <summary>
+    /// 粉ものの投入処理
+    /// </summary>
+    /// <param name="powder"></param>
+    private void ThrowIn(Powder powder)
+    {
+        contentsID.Enqueue((uint)powder.type);
+    }
+}
---
+++ VR D/Assets/Scripts/GameObjects/Powder.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Powder : MonoBehaviour
+{
+    public enum Type : uint
+    {
+        Sugar = 0,
+        BlackSuger
+    }
+    [SerializeField] Type type_;
+    public Type type;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
