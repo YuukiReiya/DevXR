30461e19ec4f604a156b54e2c11ff86c24f4ecc9
saki_bloom <toki.ryuka.senriyama@gmail.com>
2019-11-11 15:35:30 +0900

    粉スクリプトの更新。

--- VR D/Assets/Scripts/GameObjects/Powder.cs
+++ VR D/Assets/Scripts/GameObjects/Powder.cs
@@ -1,7 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using Yuuki.MethodExpansions;
 public class Powder : MonoBehaviour
 {
     public enum Type : uint
@@ -9,18 +9,79 @@ public class Powder : MonoBehaviour
         Sugar = 0,
         BlackSuger
     }
+    //serialize param
     [SerializeField] Type type_;
-    public Type type;
+    [SerializeField] GameObject instantiatePositionObject_;
+    [SerializeField] ParticleSystem effect_;
+    //private param
+    IEnumerator[] routines;
+    const int c_RoutineCount = 5;
+    //public param
+    //accessor
+    public Type type { get { return type_; } }
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        routines = new IEnumerator[c_RoutineCount];
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        Shake();
+    }
+
+    /// <summary>
+    /// 振った時の処理
+    /// </summary>
+    void Shake()
+    {
+        if(IsShake())
+        {
+            for (int i = 0; i < routines.Length; ++i)
+            {
+                if (routines[i] != null) { continue; }
+                routines[i] = MainRoutine();
+                Debug.LogError("endf funvc:" + i);
+                int index = i;
+                this.StartCoroutine(routines[i], () => { ResetRoutine(index); });
+            }
+        }
+    }
+    /// <summary>
+    /// 振られたか判定
+    /// </summary>
+    bool IsShake()
+    {
+        //オブジェクトの上向きベクトルを取得
+        var upvec = this.gameObject.transform.up.normalized;
+        var angle = Vector3.Angle(upvec, Vector3.up);
+
+        //TODO:0～360と-180～180で処理が変わってしまう？(前者な気がする)
+        Debug.Log("2つのなす角 ＝ " + angle);
+
+        //傾いているか
+        if (!(angle > 90)) { return false; }
+
+
+        return true;
+    }
+
+    IEnumerator MainRoutine()
+    {
+        var effect = PowderPool.Instance.GetObject().GetComponent<ParticleSystem>();
+        effect.gameObject.transform.parent = instantiatePositionObject_.transform;
+        effect.gameObject.transform.position = instantiatePositionObject_.transform.position;
+        effect.transform.localScale = Vector3.one;
+        effect.Play();
+        yield return new WaitWhile(() => effect.IsAlive(true));
+        effect.transform.parent = PowderPool.Instance.gameObject.transform;
+        effect.gameObject.SetActive(false);
+    }
+
+    void ResetRoutine(int index)
+    {
+        routines[index] = null;
     }
 }
