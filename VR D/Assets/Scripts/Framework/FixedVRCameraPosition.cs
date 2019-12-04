6216f07ac92fce57bd4e092d040564f41178527b
saki_bloom <toki.ryuka.senriyama@gmail.com>
2019-12-04 12:28:42 +0900

    ボタンの長押しでカメラの位置リセット機能追加。

--- VR D/Assets/Scripts/Framework/FixedVRCameraPosition.cs
+++ VR D/Assets/Scripts/Framework/FixedVRCameraPosition.cs
@@ -2,13 +2,15 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.XR;
+using Yuuki.MethodExpansions;
 
 //TODO:カメラの位置を固定するコードを書く
 public class FixedVRCameraPosition : MonoBehaviour
 {
     [SerializeField] Vector3 setupPosition;
     [SerializeField] Camera target;
-
+    [SerializeField] float resetTime;
+    IEnumerator routine;
 #if UNITY_EDITOR
     [SerializeField] bool isUpdateRecenterPosition = false;
 #endif
@@ -21,6 +23,25 @@ public class FixedVRCameraPosition : MonoBehaviour
 
     private void Update()
     {
+        #region メニュー長押しでカメラ位置のリセット
+        if(OVRInput.Get(OVRInput.RawButton.Start))
+        {
+            if (routine == null)
+            {
+                routine = ResetCameraCenterPosition();
+                this.StartCoroutine(routine, () => { routine = null; });
+            }
+        }
+        else
+        {
+            if (routine != null)
+            {
+                StopCoroutine(routine);
+                routine = null;
+            }
+        }
+        #endregion
+
 #if UNITY_EDITOR
         if (!isUpdateRecenterPosition) { return; }
         if(OVRInput.Get(OVRInput.Button.Any))
@@ -28,5 +49,16 @@ public class FixedVRCameraPosition : MonoBehaviour
             OVRManager.display.RecenterPose();
         }
 #endif
+    }
+
+    IEnumerator ResetCameraCenterPosition()
+    {
+        var time = Time.time;
+        while (Time.time < time + resetTime)
+        {
+            yield return null;
+        }
+        OVRManager.display.RecenterPose();
+        yield break;
     }
 }
